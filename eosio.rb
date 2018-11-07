666285737dece741194cbc8c94707aded7af239d
Bucky Kittinger <larrykittinger@gmail.com>
2018-11-06 20:46:21 -0500

    Version 1.4.3

--- eosio.rb
+++ eosio.rb
@@ -2,8 +2,8 @@ class Eosio < Formula
 
    homepage "https://github.com/eosio/eos"
    revision 0
-   url "https://github.com/eosio/eos/archive/v1.4.2.tar.gz"
-   version "1.4.2"
+   url "https://github.com/eosio/eos/archive/v1.4.3.tar.gz"
+   version "1.4.3"
    
    option :universal
 
@@ -14,9 +14,9 @@ class Eosio < Formula
    depends_on :arch =>  :intel
   
    bottle do
-      root_url "https://github.com/eosio/eos/releases/download/v1.4.2"
-      sha256 "465d1e9674ba530a63ea05548cc4bfe01ef564a3f4b842c24a21aca0a5cc3c50" => :high_sierra
-      sha256 "969b018633ce59064ef956123bc051e85b9fc8b7f7c88804a06cda3ed2a27792" => :mojave
+      root_url "https://github.com/eosio/eos/releases/download/v1.4.3"
+      sha256 "0d80b7d6d439f0c561a358ccc1b25199c9301efa13637d073ebb33d1719dc51e" => :high_sierra
+      sha256 "b5f99b768c45025eb91f244d2f1449d1622a7a75fda73e4ece66e6699026783a" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
