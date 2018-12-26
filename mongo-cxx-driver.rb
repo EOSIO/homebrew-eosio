73467bb3335c2d8748aa05069db09768983e623b
Nathan Pierce <NorseGaud@users.noreply.github.com>
2018-12-26 11:54:33 -0500

    Removed bottle download to silence 404

--- mongo-cxx-driver.rb
+++ mongo-cxx-driver.rb
@@ -1,16 +1,9 @@
 class MongoCxxDriver < Formula
-  desc "C++ driver for MongoDB"
+  desc "EOSIO C++ driver for MongoDB"
   homepage "https://github.com/mongodb/mongo-cxx-driver"
   url "https://github.com/mongodb/mongo-cxx-driver/archive/r3.4.0.tar.gz"
   sha256 "e9772ac5cf1c996c2f77fd78e25aaf74a2abf5f3864cb31b18d64955fd41c14d"
   head "https://github.com/mongodb/mongo-cxx-driver.git"
-
-  bottle do
-    cellar :any
-    sha256 "da2aacd94c60bbdd1c4f7b4c0103ac90857d41733f0d95666370a878539a9084" => :mojave
-    sha256 "f08a8bc08e9320b81a2142b100a43cc40aa010040c01815f345f9556d45e7a41" => :high_sierra
-    sha256 "98fec6ef3256c3955dd29fcfe825faf575f9d71f0f3061b65be62f2b558148ed" => :sierra
-  end
 
   depends_on "cmake" => :build
   depends_on "mongo-c-driver"
