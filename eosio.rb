a7abda78157f4fd25b6cfee04002a5abf9075ecf
Block.one DevOps <44441331+blockone-devops@users.noreply.github.com>
2019-04-03 12:49:24 -0400

    Update brew file for new release

--- eosio.rb
+++ eosio.rb
@@ -2,8 +2,8 @@ class Eosio < Formula
 
    homepage "https://github.com/eosio/eos"
    revision 0
-   url "https://github.com/eosio/eos/archive/v1.7.0.tar.gz"
-   version "1.7.0"
+   url "https://github.com/eosio/eos/archive/v1.7.1.tar.gz"
+   version "1.7.1"
 
    option :universal
 
@@ -15,9 +15,9 @@ class Eosio < Formula
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/eosio/eos/releases/download/v1.7.0"
-      sha256 "a7a07d94da8b0de1f7ba93d1601e35ab16633256562a8e1c490c750de495d57b" => :high_sierra
-      sha256 "aa01b4bc4fb0c06dfdbfb849cb5e37ecd8a0f3277dce8798ab239bd4322e8abd" => :mojave
+      root_url "https://github.com/eosio/eos/releases/download/v1.7.1"
+      sha256 "36a2ed761fd08d4fe8875021f1ff62728f2b7edfdb83df972e46c39e56da6441" => :high_sierra
+      sha256 "cf1d98ca32c7fb7c62c7267452ccbaec25dffa44fd0219cbf722c29329ed3b76" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
