68b6a76ccde1744aa9b706cba7be61c26f0a485f
Scott Arnette <scott.a.arnette@gmail.com>
2019-12-17 17:07:52 -0500

    Work in progress.

--- .buildkite/pr-branch.sh
+++ .buildkite/pr-branch.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -eo pipefail
 
-ls -la
-git status
+# ls -la
+# git status
+
+for DEP in $(ls *.rb | grep -v -e eosio -e doxygen); do
+    for ARTIFACT in $(ls *.tar.gz | grep -e ${DEP%.*}); do
+        HASH=$(shasum -a 256 $ARTIFACT )
+        [[ $ARTIFACT =~ 'mojave' ]] && OSX_VERSION='mojave' || OSX_VERSION='catalina'
+        LINE="sha256 \"$HASH\" => :$OSX_VERSION"
+        echo $DEP $ARTIFACT $LINE
+    done
+    # Write $LINE to appropriate location in file.
+    awk -v awkvar="$LINE" '/cellar/ {print "    "awkvar;} 1' $DEP >> $DEP
+done
+
+# Push new branch named 'cicd-$BUILDKITE_BRANCH' goes here.
+# git status
