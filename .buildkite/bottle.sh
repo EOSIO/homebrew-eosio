5fb3439ab8762a599d58ff898384e6338c8658d9
Scott Arnette <scott.a.arnette@gmail.com>
2019-12-17 11:57:19 -0500

    Split into builds per OSX distro.

--- .buildkite/bottle.sh
+++ .buildkite/bottle.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -eo pipefail
+GIT_DIR=$(pwd)
 
-for filename in *.tar.gz; do 
-    echo $filename; mv $filename ${filename/-/}
+brew tap eosio/eosio
+cd /usr/local/Homebrew/Library/Taps/eosio/homebrew-eosio
+git pull origin $BUILDKITE_BRANCH && git checkout -f $BUILDKITE_COMMIT
+cd $GIT_DIR
+
+for dep in $(ls *.rb | grep -v -e eosio -e doxygen); do
+    brew install --build-bottle eosio/eosio/${dep%.*}
+    brew bottle eosio/eosio/${dep%.*}
+    brew postinstall eosio/eosio/${dep%.*}
+done
+
+for file in *.tar.gz; do 
+    mv $file ${file/-/}
 done
