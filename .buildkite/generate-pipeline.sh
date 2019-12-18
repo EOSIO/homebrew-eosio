bfb88099fbf05ebe8106a8f4dac66120d972d63c
Scott Arnette <scott.a.arnette@gmail.com>
2019-12-18 15:33:15 -0500

    Fix for steps on fleet change.

--- .buildkite/generate-pipeline.sh
+++ .buildkite/generate-pipeline.sh
@@ -62,16 +62,16 @@ cat <<EOF
   - label: ":rocket: Create Branch for PR"
     command:
       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-      - "git clone $BUILDKITE_REPO homebrew-eosio && cd homebrew-eosio && git checkout $BUILDKITE_BRANCH"
+      - "git clone $BUILDKITE_REPO . && git checkout $BUILDKITE_BRANCH"
 EOF
 for DEP in $(ls *.rb | grep -v -e eosio -e doxygen); do
     cat <<EOF
-      - "cd homebrew-eosio && buildkite-agent artifact download '*.tar.gz' . --step ':hammer: Mojave Bottle | ${DEP%.*}'"
-      - "cd homebrew-eosio && buildkite-agent artifact download '*.tar.gz' . --step ':hammer: Catalina Bottle | ${DEP%.*}'"
+      - "buildkite-agent artifact download '*.tar.gz' . --step ':hammer: Mojave Bottle | ${DEP%.*}'"
+      - "buildkite-agent artifact download '*.tar.gz' . --step ':hammer: Catalina Bottle | ${DEP%.*}'"
 EOF
 done
 cat <<EOF
-      - "cd homebrew-eosio && ./.buildkite/pr-branch.sh"
+      - "./.buildkite/pr-branch.sh"
     if: build.branch !~ /^cicd-/
     plugins:
       - thedyrt/skip-checkout#v0.1.1:
