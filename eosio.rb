844a5725433f43b079ab5e06c6dfe51104b0f296
arhag <arhag@users.noreply.github.com>
2019-12-17 20:38:37 -0500

    Update brew file for new release

--- eosio.rb
+++ eosio.rb
@@ -2,21 +2,21 @@ class Eosio < Formula
 
    homepage "https://github.com/eosio/eos"
    revision 0
-   url "https://github.com/eosio/eos/archive/v1.8.6.tar.gz"
-   version "1.8.6"
+   url "https://github.com/eosio/eos/archive/v1.8.7.tar.gz"
+   version "1.8.7"
 
    option :universal
 
    depends_on "gmp"
    depends_on "gettext"
-   depends_on "openssl"
+   depends_on "openssl@1.1"
    depends_on "libusb"
    depends_on :macos => :mojave
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/eosio/eos/releases/download/v1.8.6"
-      sha256 "98f87d631420ffd576779398db4f31931857654b9b7be07ef637383460eb827d" => :mojave
+      root_url "https://github.com/eosio/eos/releases/download/v1.8.7"
+      sha256 "06535deeeef7c11ef26ca0915e9715c70baa4ac7296d0840cf9d9d3687caed65" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
