86323422eecebc4e37a124f569d755efa2105e58
Victor Camacho <victor.camacho@block.one>
2021-03-18 16:41:11 -0400

    Update brew file for new release

--- eosio.rb
+++ eosio.rb
@@ -2,8 +2,8 @@ class Eosio < Formula
 
    homepage "https://github.com/eosio/eos"
    revision 0
-   url "https://github.com/eosio/eos/archive/v2.0.10.tar.gz"
-   version "2.0.10"
+   url "https://github.com/eosio/eos/archive/v2.0.11.tar.gz"
+   version "2.0.11"
 
    option :universal
 
@@ -15,8 +15,8 @@ class Eosio < Formula
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/eosio/eos/releases/download/v2.0.10"
-      sha256 "43ec0a26026efe2f8c1abe28a1209daab4e904a795c104b7a6c0490a66248f94" => :mojave
+      root_url "https://github.com/eosio/eos/releases/download/v2.0.11"
+      sha256 "a17e43ed61c665be93b93b20f794e451197c0b7756bc7632e740e744f4a4ef05" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
