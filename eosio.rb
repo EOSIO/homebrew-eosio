ae45b3e56926351e855f3b8d22222a0a7141a1db
killshot13 <dmreh@outlook.com>
2021-05-20 21:58:55 -0500

    Fix(brew): Modified post-merge commit

--- eosio.rb
+++ eosio.rb
@@ -1,15 +1,13 @@
 # typed: false
 # frozen_string_literal: true
+class Eosio < Formula
 
    homepage "https://github.com/eosio/eos"
    revision 0
    url "https://github.com/eosio/eos/archive/v2.1.0.tar.gz"
    version "2.1.0"
 
-  bottle do
-    root_url "https://github.com/eosio/eos/releases/download/v2.0.12"
-    sha256 mojave: "2e03b4fe73e0cdc70e607b35e35294f49d6833439176b4dedc846e62a3b77005"
-  end
+option :universal
 
    depends_on "gmp"
    depends_on "openssl@1.1"
