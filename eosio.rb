1bfa23048b8901ff39ab80b47b5749d3f3e614d7
EOSIO Automation <36459142+eosio-cicd@users.noreply.github.com>
2021-06-22 07:55:06 -0400

    Update brew file for new release

--- eosio.rb
+++ eosio.rb
@@ -16,7 +16,7 @@ class Eosio < Formula
 
    bottle do
       root_url "https://github.com/eosio/eos/releases/download/v2.1.0"
-      sha256 "b1562e0e2d97288104839884871212d4c2ab1b54b0764aa9bb5f24bb1d5d37ab" => :catalina
+      sha256 "52aa4e6e46388c0c944d7e798ecd6f144f311eebde07bcd3e9139e57dff9aabd" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
