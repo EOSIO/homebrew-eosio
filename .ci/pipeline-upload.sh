cd0c51c1e8c014d2530ed784288ebc4a70d22e29
Zach Butler <zach.butler@block.one>
2021-07-13 00:38:30 -0400

    Cross-reference bottle SHA-256 with ruby file

--- .ci/pipeline-upload.sh
+++ .ci/pipeline-upload.sh
@@ -6,6 +6,7 @@ export AMKA_VM_MAP='{"mojave":"10.14.6_6C_14G_80G","catalina":"10.15.5_6C_14G_80
 export PACKAGE='eosio'
 export REPO_UNDER_TEST='git@github.com:EOSIO/eos.git'
 export RETRY="$([[ "$BUILDKITE" == 'true' ]] && buildkite-agent meta-data get pipeline-upload-retries --default '0' || echo '0')"
+export ROOT_URL="$(cat "$RUBY_FILE" | grep -P '^\s+root_url' | head -1 | awk '{print $2}' | tr -d '"')"
 echo "Retry number $RETRY." >&2
 export RUBY_FILE='eosio.rb'
 export TAG="v$(cat "$RUBY_FILE" | grep -P '^\s+version' | awk '{print $2}' | tr -d '"')"
@@ -33,11 +34,12 @@ EOF
 # parse ruby file and generate pipeline steps
 export LABEL='Homebrew Package Validation'
 for OS in $(cat "$RUBY_FILE" | grep -P '^\s+sha256' | awk '{print $2}' | tr -d ':'); do
+    export BOTTLE="eosio-$(echo "$TAG" | tr -d 'v').$OS.bottle.tar.gz"
     export OS="$OS" # for jq env command
     export OS_STYLIZED="$(echo "$OS" | sed -e 's/\b\(.\)/\u\1/g')"
     echo "Found $OS operating system." >&2
     export VM="$(echo "$AMKA_VM_MAP" | jq -r '.[env.OS]')"
-    cat .ci/anka.yml | envsubst '$ANKA_LAYERS $BUILDKITE_PIPELINE_SLUG $BUILDKITE_REPO $LABEL $OS_STYLIZED $PACKAGE $REPO_UNDER_TEST $TAG $TAP $VM'
+    cat .ci/anka.yml | envsubst '$ANKA_LAYERS $BOTTLE $BUILDKITE_PIPELINE_SLUG $BUILDKITE_REPO $LABEL $OS $OS_STYLIZED $PACKAGE $REPO_UNDER_TEST $ROOT_URL $RUBY_FILE $TAG $TAP $VM'
 done
 echo '--- :white_check_mark: Done! Good luck :)' >&2
 echo 'Done.' >&2
--- .ci/test.sh
+++ .ci/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 echo '--- :evergreen_tree: Configuring Environment'
+exec 9>&1 # enable tee to write to STDOUT as a file
 echo "Using git tag '$REPO_UNDER_TEST:$GIT_TAG'."
 CLONE="git clone --recursive --single-branch --branch '${GIT_TAG}' '${REPO_UNDER_TEST}'"
 echo "$ $CLONE"
@@ -8,6 +9,27 @@ eval $CLONE
 BREW_UPDATE='brew update'
 echo "$ $BREW_UPDATE"
 eval $BREW_UPDATE
+if [[ ! -z "$BOTTLE" ]]; then
+    echo '+++ :hash: Verify Package Hash - SHA-256'
+    DOWNLOAD="curl -fsSL -o './$BOTTLE' '$ROOT_URL/$BOTTLE'"
+    echo "$ $DOWNLOAD"
+    eval $DOWNLOAD
+    FILE_HASH="openssl dgst -sha256 '$BOTTLE'"
+    echo "$ $FILE_HASH"
+    BOTTLE_HASH="$(eval $FILE_HASH | tee >(cat - >&9) | awk '{print $2}')"
+    RUBY_LINE="cat '$RUBY_FILE' | grep -P 'sha256.*$OS'"
+    echo "$ $RUBY_LINE"
+    RUBY_HASH="$(eval $RUBY_LINE | tee >(cat - >&9) | awk '{print $3}' | tr -d '"')"
+    if [[ "$BOTTLE_HASH" == "$RUBY_HASH" ]]; then
+        echo "Pass: $BOTTLE_HASH = $RUBY_HASH"
+    else
+        echo "+++ :x: Failed to Verify Bottle Hash"
+        FAIL_MSG="$BOTTLE hash $BOTTLE_HASH ≠ $RUBY_FILE::$OS hash $RUBY_HASH"'!'
+        echo "FAILURE: $FAIL_MSG"
+        [[ "$BUILDKITE" == 'true' ]] && echo "**FAILURE:** $FAIL_MSG" | buildkite-agent annotate --style 'error' --context "wrong-hash-$OS"
+        exit 2
+    fi
+fi
 echo '+++ :beer: Homebrew Tap and Install'
 BREW_TAP="brew tap '$TAP'"
 echo "$ $BREW_TAP"
