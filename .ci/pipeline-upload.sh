8151dcf49c9971206f2020f326521e707c2cc875
Zach Butler <zach.butler@block.one>
2021-07-12 23:18:21 -0400

    Parameterize repo

--- .ci/pipeline-upload.sh
+++ .ci/pipeline-upload.sh
@@ -4,6 +4,7 @@ echo '+++ :evergreen_tree: Configuring Environment' >&2
 export ANKA_LAYERS='clean::cicd::git-ssh::nas::brew::buildkite-agent'
 export AMKA_VM_MAP='{"mojave":"10.14.6_6C_14G_80G","catalina":"10.15.5_6C_14G_80G","big_sur":"11.2.1_6C_14G_80G"}'
 export PACKAGE='eosio'
+export REPO_UNDER_TEST='git@github.com:EOSIO/eos.git'
 export RETRY="$([[ "$BUILDKITE" == 'true' ]] && buildkite-agent meta-data get pipeline-upload-retries --default '0' || echo '0')"
 echo "Retry number $RETRY." >&2
 export RUBY_FILE='eosio.rb'
@@ -36,7 +37,7 @@ for OS in $(cat "$RUBY_FILE" | grep -P '^\s+sha256' | awk '{print $2}' | tr -d '
     export OS_STYLIZED="$(echo "$OS" | sed -e 's/\b\(.\)/\u\1/g')"
     echo "Found $OS operating system." >&2
     export VM="$(echo "$AMKA_VM_MAP" | jq -r '.[env.OS]')"
-    cat .ci/anka.yml | envsubst '$ANKA_LAYERS $BUILDKITE_PIPELINE_SLUG $BUILDKITE_REPO $LABEL $OS_STYLIZED $PACKAGE $TAG $TAP $VM'
+    cat .ci/anka.yml | envsubst '$ANKA_LAYERS $BUILDKITE_PIPELINE_SLUG $BUILDKITE_REPO $LABEL $OS_STYLIZED $PACKAGE $REPO_UNDER_TEST $TAG $TAP $VM'
 done
 echo '--- :white_check_mark: Done! Good luck :)' >&2
 echo 'Done.' >&2
--- .ci/test.sh
+++ .ci/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eo pipefail
 echo '--- :evergreen_tree: Configuring Environment'
-echo "Using git tag 'eos:$GIT_TAG'."
-CLONE="git clone --recursive --single-branch --branch '${GIT_TAG}' 'git@github.com:EOSIO/eos.git'"
+echo "Using git tag '$REPO_UNDER_TEST:$GIT_TAG'."
+CLONE="git clone --recursive --single-branch --branch '${GIT_TAG}' '${REPO_UNDER_TEST}'"
 echo "$ $CLONE"
 eval $CLONE
 BREW_UPDATE='brew update'
@@ -20,7 +20,7 @@ cd eos
 export BUILD_ROOT="$(which nodeos | sed 's_/bin/nodeos__g')"
 echo "Found nodeos at \"$BUILD_ROOT/bin/nodeos\"."
 export BUILDKITE_COMMIT="$(git rev-parse HEAD)" # override homebrew-eosio commit with eos tag commit
-echo "Found 'eos:$BUILDKITE_COMMIT'."
+echo "Found git commit '$REPO_UNDER_TEST:$BUILDKITE_COMMIT'."
 unset BUILDKITE_TAG
 export CMAKE_SOURCE_DIR="$(pwd)"
 echo "Set CMAKE_SOURCE_DIR to \"$CMAKE_SOURCE_DIR\"."
