5b78e6d5a700f9e239cfee17231ba0bb6d3b862e
Zach Butler <zach.butler@block.one>
2021-07-14 12:56:22 -0400

    Fail when we cannot find the nodeos binary

--- .ci/test.sh
+++ .ci/test.sh
@@ -44,7 +44,14 @@ cd eos
 WHICH_BIN="which '$BIN'"
 echo "$ $WHICH_BIN"
 export BUILD_ROOT="$(eval $WHICH_BIN | tee >(cat - >&9) | sed "s_/bin/${BIN}__g")"
-echo "Found '$BIN' at \"$BUILD_ROOT/bin/$BIN\"."
+if [[ -z "$BUILD_ROOT" ]]; then
+    FAIL_MSG="'$BIN' binary not found after homebrew installation on $OS_STYLIZED"'!'
+    echo "FAILURE: $FAIL_MSG"
+    [[ "$BUILDKITE" == 'true' ]] && echo "**FAILURE:** $FAIL_MSG" | buildkite-agent annotate --style 'error' --context "no-binary-$OS"
+    exit 3
+else
+    echo "Found '$BIN' at \"$BUILD_ROOT/bin/$BIN\"."
+fi
 export BUILDKITE_COMMIT="$(git rev-parse HEAD)" # override homebrew-eosio commit with eos tag commit
 echo "Found git commit '$REPO_UNDER_TEST:$BUILDKITE_COMMIT'."
 unset BUILDKITE_TAG
