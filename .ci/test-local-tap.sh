a04c9b9e7c9adf1c46cc8405762aa72ee1be41c4
William Blevins <william.blevins@block.one>
2021-08-04 13:00:29 -0400

    Optimize use case from eosio package tests.

--- .ci/test-local-tap.sh
+++ .ci/test-local-tap.sh
@@ -5,20 +5,12 @@ set -eo pipefail
 
 echo '--- :evergreen_tree: Configuring Environment'
 exec 9>&1 # enable tee to write to STDOUT as a file
-test_bottle
+[[ -n "$BOTTLE" ]] && test_bottle
 
 echo '+++ :beer: Homebrew Tap and Install'
 perform 'brew update'
-if [[ ! $(python3 --version 2>/dev/null) ]]; then
-   perform 'brew install python3'
-fi
-perform "sed -i.bk -e 's/url \".*\"/url \"http:\/\/127.0.0.1:7800\"/' ${BUILDKITE_PIPELINE_SLUG}/*.rb"
-perform "pushd ${BUILDKITE_PIPELINE_SLUG} && git add *.rb && git commit -m 'test it!' && popd"
 perform "brew tap eosio/eosio ${BUILDKITE_PIPELINE_SLUG}"
-perform '{ python3 -m http.server 7800 & } && export HTTP_SERVER_PID=$!'
-perform 'sleep 20s'
-perform 'brew install eosio'
-perform 'kill $HTTP_SERVER_PID'
+perform "brew install '$PACKAGE'"
 
 echo '+++ :label: Full Version Label Test'
 test_binary_exists
